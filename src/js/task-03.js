e30c499d82c66a847e4036d6d2d0f352f118b627
Anna <73854503+Blossom0204@users.noreply.github.com>
2021-04-04 21:52:16 +0300

    Решение задачи 3

---
+++ src/js/task-03.js
@@ -0,0 +1,27 @@
+const images = [
+    {
+      url:
+        'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'White and Black Long Fur Cat',
+    },
+    {
+      url:
+        'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    },
+    {
+      url:
+        'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'Group of Horses Running',
+    },
+  ];
+  const ulList = document.querySelector('#gallery');
+  const newList = images.map(img => {
+    const item = document.createElement('li');
+    const image = document.createElement('img');
+    image.setAttribute('src', `${img.url}`);
+    image.setAttribute('alt', `${img.alt}`);
+    item.append(image)
+    return item;
+      });
+  ulList.append(...newList);
