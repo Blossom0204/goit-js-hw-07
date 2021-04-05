1734c38b10a0c44dcb8c8d83f2966c7856de32f6
Anna <73854503+Blossom0204@users.noreply.github.com>
2021-04-05 21:33:29 +0300

    Решение задачи 7

---
+++ src/js/task-07.js
@@ -0,0 +1,4 @@
+const slider = document.querySelector("#font-size-control");
+const text = document.querySelector("#text");
+slider.addEventListener('input', () => {
+text.style.fontSize = slider.value + "px"});
